6cafa9c3428601d2057ebbbf5e15dd411b016dba
tatianaD <angietatiana167@gmail.com>
2024-06-06 20:14:38 +0200

    on-off

--- js/main.js
+++ js/main.js
@@ -1,23 +1,29 @@
 let interruptor = false;
 let channels = document.getElementsByClassName("channels");
 let power = document.getElementById("power");
+let tv = document.getElementById("tv")
 
 
 let arrayBotones = Array.from(channels)
 console.log(arrayBotones)
 
 
-
-
 for(let i = 0; i < arrayBotones.length; i++){
     arrayBotones[i].addEventListener("click", () => {
         if(interruptor){
-            console.log(`Has pulsado el botón ${i+1}`)
+            tv.classList.remove(tv.classList[tv.classList.length-1])
+            tv.classList.add(`channel${i}`)
+           
+        console.log(`Has pulsado el botón ${i+1}`)
         }
     })
 }
 power.addEventListener("click", ()=> {
     console.log("me han pulsado....")
     interruptor = !interruptor
+    if (interruptor){tv.classList.add("channel0")}
+
+    else {tv.classList.remove(tv.classList[tv.classList.length-1])}
+
     power.classList.toggle("power-on")
 })
