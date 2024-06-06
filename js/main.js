5e81523bcbc94b2b65137487aa1ad8ef7210ed22
tatianaD <angietatiana167@gmail.com>
2024-06-06 12:04:45 +0200

    insert imag

--- js/main.js
+++ js/main.js
@@ -1,20 +1,23 @@
-let interruptor = false; 
+let interruptor = false;
 let channels = document.getElementsByClassName("channels");
-let buttons = document.getElementsByClassName("power");
+let power = document.getElementById("power");
 
 
-let arrayBotones = Array.from(buttons)
+let arrayBotones = Array.from(channels)
 console.log(arrayBotones)
+
+
 
 
 for(let i = 0; i < arrayBotones.length; i++){
     arrayBotones[i].addEventListener("click", () => {
         if(interruptor){
             console.log(`Has pulsado el botón ${i+1}`)
-            
-        }   
+        }
     })
 }
-    
-   
-
+power.addEventListener("click", ()=> {
+    console.log("me han pulsado....")
+    interruptor = !interruptor
+    power.classList.toggle("power-on")
+})
